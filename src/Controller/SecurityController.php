02d7800bae1eff67a13de5271227ead702633fcd
AndreyDuda <AndreyDuda88@gmail.com>
2019-05-04 16:33:47 +0300

    registers

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -21,4 +21,12 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout()
+    {
+        throw new \Exception('Don t forget to activate logout in security.yaml');
+    }
 }

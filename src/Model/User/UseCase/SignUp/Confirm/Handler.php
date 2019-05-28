796e2dbd62fbed392db41bac775557a7ef5d212d
AndreyDuda <AndreyDuda88@gmail.com>
2019-05-28 21:18:30 +0300

    correct work app 2

--- src/Model/User/UseCase/SignUp/Confirm/Handler.php
+++ src/Model/User/UseCase/SignUp/Confirm/Handler.php
@@ -7,6 +7,9 @@ use App\Model\User\Entity\User\UserRepository;
 
 class Handler
 {
+	/**
+	 * @var UserRepository
+	 */
 	private $users;
 	private $flusher;
 	
@@ -15,4 +18,15 @@ class Handler
 		$this->users = $users;
 		$this->flusher = $flusher;
 	}
+	
+	public function handler(Command $command): void
+	{
+		if (!$user = $this->users->findByConfrmToken($command->token)) {
+			throw new \DomainException('Incorrect or confirm token.');
+		}
+		
+		$user->confirmSignUp();
+		$this->flusher->flush();
+	}
+	
 }
